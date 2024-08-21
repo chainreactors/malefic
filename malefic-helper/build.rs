d54f3eb4e782ccb333cc56bc73a2fa9093a72c62
h3zh1 <h3zh1@qq.com>
2024-08-21 17:19:54 +0800

    Fix: support win32 for commit 9cf1179c6

--- malefic-helper/build.rs
+++ malefic-helper/build.rs
@@ -9,18 +9,23 @@ lazy_static! {
 
 fn main() {
     #[cfg(feature = "community")]
-    #[cfg(target_os = "windows")]
     {
-        let manifest_dir = env::var("CARGO_MANIFEST_DIR")
-        .expect("CARGO_MANIFEST_DIR environment variable not set");
-        // let lib_path = PathBuf::from(manifest_dir).join("..\\resources");
-        let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-        #[cfg(target_arch="x86_64")]
-        std::fs::copy("../resources/libmalefic_win_kit.a", out_dir.join("libmalefic_win_kit.a")).unwrap();
-        #[cfg(target_arch="x86")]
-        std::fs::copy("../resources/libmalefic_win_kit32.a", out_dir.join("libmalefic_win_kit.a")).unwrap();
-        println!("cargo:rustc-link-search=native={}", out_dir.display());
-        println!("cargo:rustc-link-lib=static={}", "malefic_win_kit");
+        loop {
+            if std::env::var("CARGO_CFG_TARGET_OS").unwrap() != "windows" {
+                break;
+            }
+            let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+            if std::env::var("CARGO_CFG_TARGET_ARCH").unwrap() == "x86_64" {
+                std::fs::copy("../resources/libmalefic_win_kit.a", out_dir.join("libmalefic_win_kit.a")).unwrap();
+            } else if std::env::var("CARGO_CFG_TARGET_ARCH").unwrap() == "x86" {
+                std::fs::copy("../resources/libmalefic_win_kit32.a", out_dir.join("libmalefic_win_kit.a")).unwrap();
+            } else {
+                break;
+            }
+            println!("cargo:rustc-link-search=native={}", out_dir.display());
+            println!("cargo:rustc-link-lib=static={}", "malefic_win_kit");
+            break;
+        }
     }
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=path/to/Cargo.lock");
