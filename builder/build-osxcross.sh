423ff00c4ecad67c150829b3404949fb05c802c8
h3zh1 <h3zh1@qq.com>
2024-08-23 02:00:32 +0800

    Fix: update TP_OSXCROSS_DEV=/build

--- builder/build-osxcross.sh
+++ builder/build-osxcross.sh
@@ -3,5 +3,5 @@ if [ -e "/System" ]; then
     exit 0
 fi
 cd /build/osxcross
-TP_OSXCROSS_DEV=/build/osxcross/build SDK_VERSION=11.3 UNATTENDED=yes OSX_VERSION_MIN=10.13 ./build.sh
+TP_OSXCROSS_DEV=/build SDK_VERSION=11.3 UNATTENDED=yes OSX_VERSION_MIN=10.13 ./build.sh
 ln -s /build/osxcross/target/SDK/MacOSX11.3.sdk/System/ /System
