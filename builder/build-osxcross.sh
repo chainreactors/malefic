6215e63ffe5e7a87c8ef5c300b467af364a877aa
h3zh1 <h3zh1@qq.com>
2024-09-23 22:27:31 +0800

    Remove unnecessary files

--- builder/build-osxcross.sh
+++
@@ -1,7 +0,0 @@
-if [ -e "/System" ]; then
-    echo "/osxcross already build. Skipping the build process..."
-    exit 0
-fi
-cd /build/osxcross
-TP_OSXCROSS_DEV=/build SDK_VERSION=11.3 UNATTENDED=yes OSX_VERSION_MIN=10.13 ./build.sh
-ln -s /build/osxcross/target/SDK/MacOSX11.3.sdk/System/ /System
