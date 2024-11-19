12ca2cef5aecc9272530a0f1110bc4a26881cf59
h3zh1 <h3zh1@qq.com>
2024-11-20 00:41:22 +0800

    feat: update github release

--- remap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-output_file="remap-path.env"
-echo -n 'RUSTFLAGS="-A warnings' > $output_file
-find ./ -type f -path "./malefic*" -name "*.rs" | sed 's|^\./||' | while read rs_file; do
-    random_value=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
-    echo -n "--remap-path-prefix=${rs_file}=${random_value}.rs " >> $output_file
-done
-echo -n '"' >> $output_file
-echo "Generated $output_file"
