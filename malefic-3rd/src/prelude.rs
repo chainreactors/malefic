092787880bf8bca5edd7f0e280d97cab89f86c84
M09Ic <m09ician@gmail.com>
2025-08-17 16:30:13 +0800

    merge: merge v0.1.2_rc1

---
+++ malefic-3rd/src/prelude.rs
@@ -0,0 +1,3 @@
+pub use malefic_trait::module_impl;
+pub use malefic_proto::prelude::*;
+pub use anyhow::anyhow;
---
+++ malefic-proto/src/module/macro.rs
@@ -0,0 +1,82 @@
+#[macro_export]
+macro_rules! check_request {
+    ($receiver:expr, $variant:path) => {
+        match futures::StreamExt::next($receiver).await {
+            Some($variant(request)) => Ok(request),
+            _ => Err($crate::TaskError::NotExpectBody),
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! check_field {
+    ($field:expr) => {
+        if $field.is_empty() {
+            Err($crate::TaskError::FieldRequired {
+                msg: stringify!($field).to_string(),
+            })
+        } else {
+            Ok($field)
+        }
+    };
+    ($field:expr, $len:expr) => {
+        if $field.len() != $len {
+            Err($crate::TaskError::FieldLengthMismatch {
+                msg: format!("{} expected length {}", stringify!($field), $len),
+            })
+        } else {
+            Ok($field)
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! check_optional {
+    ($field:expr) => {
+        match $field {
+            Some(field) => Ok(field),
+            None => Err($crate::TaskError::FieldRequired {
+                msg: stringify!($field).to_string(),
+            }),
+        }
+    };
+    ($field:expr, $len:expr) => {
+        if $field.len() != $len {
+            Err($crate::TaskError::FieldLengthMismatch {
+                msg: format!("{} expected length {}", stringify!($field), $len),
+            })
+        } else {
+            Ok($field)
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! register_module {
+    ($map:expr, $feature:literal, $module:ty) => {
+        #[cfg(feature = $feature)]
+        {
+            let module_name = <$module as Module>::name();
+            let module_instance = <$module as Module>::new();
+            $map.insert(module_name.to_string(), Box::new(module_instance));
+        }
+    };
+}
+
+
+#[macro_export]
+macro_rules! to_error {
+    ($expr:expr) => {
+        $expr.map_err(|e| anyhow::Error::msg(format!("{:#?}", e)))
+    };
+}
+
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => {
+        #[cfg(debug_assertions)]
+        {
+            println!($($arg)*);
+        }
+    };
+}
